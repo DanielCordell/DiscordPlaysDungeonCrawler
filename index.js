9faca839b90749d5d812eded0709b9181193e4bd
Daniel Cordell <danielbc@hotmail.co.uk>
2018-11-10 11:38:54 +0000

    Discord.js test file

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const http = require('http');
-const hostname = '127.0.0.1';
-const port = 3000;
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const config = require('./config.json')
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('pong');
+  }
 });
+
+client.login(config.token);
