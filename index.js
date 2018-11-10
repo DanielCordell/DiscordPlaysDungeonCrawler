a51d0fd8e3b48a1a9329a2e499068fdc60c70024
kash <kashdadhich@gmail.com>
2018-11-10 19:02:52 +0000

    formatting

--- index.js
+++ index.js
@@ -8,33 +8,6 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-  if (msg.content === 'reacc') {
-    msg.reply('pls')
-    .then(message => {
-/*
-      const collector = message.createReactionCollector((reaction, user) => true, { time: 15000 });
-      collector.on('collect', (reaction, reactionCollector) => {
-        console.log(`Collected ${reaction.emoji.name}`);
-      });
-      collector.on('end', collected => {
-        var results = [];
-        collected.forEach(emoji => {
-          console.log(`Collected ${emoji.emoji.name}  ${emoji.count}  times`);
-          results.push({"emoji":emoji.emoji.name,"count":emoji.count});
-        });
-        max = results[0]
-        for (i=0; i<results.length; i++){
-          if (max.count < results[i].count){
-            max = results[i]
-          }
-        }
-        console.log(max.emoji, " wins the vote!");
-
-      });
-*/
-    });
-  }
-
   if (msg.content === `<@${client.user.id}> start`){
     start()
   }
@@ -75,6 +48,5 @@ function start() {
 function getChannel() {
   return client.channels.get("510773738393042986");
 }
-
 
 client.login(config.token);
